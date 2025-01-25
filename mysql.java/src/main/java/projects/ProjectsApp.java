cb1ee530b08f0db97cc562bd40ccfa9c23e6ecb1
Elliot <ellthrowaway2@icloud.com>
2025-01-25 16:36:58 -0500

    feat: week 9- create list of menu options and print to console, methods to accept user input, process input and convert to appropriate type

--- mysql.java/src/main/java/projects/ProjectsApp.java
+++ mysql.java/src/main/java/projects/ProjectsApp.java
@@ -1,9 +1,103 @@
 package projects;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+import projects.exception.DbException;
+
 public class ProjectsApp {
+	// Instance variables:
+
+	// @formatter:off
+	private List<String> operations = List.of(
+			"1) Add a project"
+			);
+	// @formatter:on
+
+	private Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
+
+		new ProjectsApp().processUserSelections();
+
+	}
+
+	private void processUserSelections() {
+		boolean done = false;
+
+		while (!done) {
+			try {
+				int selection = getUserSelection();
+			} catch (Exception e) {
+				System.out.println("\nError: " + e + " Try again.");
+			}
+		}
+	}
+
+	private int getUserSelection() {
+		printOperations();
+		Integer input = getIntInput("Enter a menu selection");
+
+		// Check for null value and return:
+		return Objects.isNull(input) ? -1 : input;
+	}
+
+	private void printOperations() {
+		System.out.println("\nThese are the available selections. Press the Enter key to quit:");
+
+		operations.forEach(line -> System.out.println("   " + line));
+	}
+
+	private Integer getIntInput(String prompt) {
+		String input = getStringInput(prompt);
+
+		// Check if input is null:
+		if (Objects.isNull(input)) {
+			return null;
+		}
 		
+		try {
+			// Convert string input to int:
+			return Integer.valueOf(input);
+		} 
+		catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid number. Try again.");
+		}
+	}
+
+	private String getStringInput(String prompt) {
+		System.out.print(prompt + ": ");
+		
+		String input = scanner.nextLine();
+		
+		// Check if input is blank, else return trimmed input:
+		return input.isBlank() ? null : input.trim();
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
