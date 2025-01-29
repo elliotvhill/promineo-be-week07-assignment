b6e6812c159092f27a5e9d3ec9a0957504d1371e
Elliot <ellthrowaway2@icloud.com>
2025-01-29 16:34:46 -0500

    feat: week 10- created method to fetch project by ID

--- mysql.java/src/main/java/projects/dao/ProjectDao.java
+++ mysql.java/src/main/java/projects/dao/ProjectDao.java
@@ -5,10 +5,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
+import projects.entity.Category;
+import projects.entity.Material;
 import projects.entity.Project;
+import projects.entity.Step;
 import projects.exception.DbException;
 import provided.util.DaoBase;
 
@@ -69,33 +75,90 @@ public class ProjectDao extends DaoBase {
 
 	public List<Project> fetchAllProjects() {
 		String sql = "SELECT * FROM " + PROJECT_TABLE + " p ORDER BY project_name";
-		
+
 		try (Connection conn = DbConnection.getConnection()) {
 			startTransaction(conn);
-			
+
 			try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-				
+
 				try (ResultSet rs = stmt.executeQuery()) {
 					List<Project> projects = new LinkedList<>();
-					
+
 					while (rs.next()) {
 						projects.add(extract(rs, Project.class));
 					}
-					
+
 					return projects;
 				}
-				
+
 			} catch (Exception e) {
 				rollbackTransaction(conn);
 				throw new DbException(e);
 			}
-			
+
 		} catch (SQLException e) {
 			throw new DbException(e);
 		}
-		
-//		return null;
+	}
 
+	public Optional<Project> fetchProjectById(Integer projectId) {
+
+		String sql = "SELECT * FROM " + PROJECT_TABLE + " WHERE project_id = ?";
+
+		// Obtain a connection
+		try (Connection conn = DbConnection.getConnection()) {
+			// Start a transaction
+			startTransaction(conn);
+
+			try {
+				Project project = null;
+
+				try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+					// Set the placeholder/parameter in the SQL statement to the passed project ID
+					setParameter(stmt, 1, projectId, Integer.class);
+
+					try (ResultSet rs = stmt.executeQuery()) {
+						// If there is a(nother) row in the ResultSet
+						if (rs.next()) {
+							// Set all Project fields to values from ResultSet
+							project = extract(rs, Project.class);
+						}
+					}
+				}
+
+				if (Objects.nonNull(project)) {
+					// Obtain list of materials, steps, and categories
+					project.getMaterials().addAll(fetchMaterialsForProject(conn, projectId));
+					project.getSteps().addAll(fetchStepsForProject(conn, projectId));
+					project.getCategories().addAll(fetchCategoriesForProject(conn, projectId));
+				}
+				
+				commitTransaction(conn);
+
+				return Optional.ofNullable(project);
+
+			} catch (Exception e) {
+				rollbackTransaction(conn);
+				throw new DbException(e);
+			}
+		} catch (SQLException e) {
+			throw new DbException(e);
+		}
+	}
+	
+	private Collection<? extends Material> fetchMaterialsForProject(Connection conn, Integer projectId) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private Collection<? extends Step> fetchStepsForProject(Connection conn, Integer projectId) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Collection<? extends Category> fetchCategoriesForProject(Connection conn, Integer projectId) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
