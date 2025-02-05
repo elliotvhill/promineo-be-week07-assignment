310d74e5d5cb8673e951317166a0b965c449fdc8
Elliot <ellthrowaway2@icloud.com>
2025-02-05 12:18:50 -0500

    week 11- added project for testing to schema

--- mysql.java/src/main/resources/projects-schema.sql
+++ mysql.java/src/main/resources/projects-schema.sql
@@ -46,3 +46,32 @@ CREATE TABLE material (
 	PRIMARY KEY (material_id),
 	FOREIGN KEY (project_id) REFERENCES project (project_id)
 );
+
+INSERT INTO project (project_name, estimated_hours, actual_hours, difficulty, notes)
+VALUES ('Build a bird house', '3', '3.5', '4', 'build a basic bird house');
+
+INSERT INTO material (project_id, material_name, num_required, cost)
+VALUES 
+	(3, 'planks of wood', 4, 20.00),
+	(3, 'glue', 1, 5),
+	(3, 'hammer', 1, null),
+	(3, 'nails', 20, 3.99);
+
+INSERT INTO step (project_id, step_text, step_order)
+VALUES
+	(3, 'Cut planks of wood to size', 1),
+	(3, 'Glue the sides of the house together', 2),
+	(3, 'Secure the sides using hammer and nails', 3),
+	(3, 'Let dry for at least 4 hours before hanging', 4);
+
+INSERT INTO category (category_id, category_name)
+VALUES
+	(3, 'Woodworking'),
+	(4, 'Crafts'),
+	(5, 'Garden');
+
+INSERT INTO project_category (project_id, category_id)
+VALUES
+	(3, 3),
+	(3, 4),
+	(3, 5);
